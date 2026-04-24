bc704ba53e065505bf91d99741a3a2c563d0d858
akandeisaac021-design <akandeisaac021@gmail.com>
2026-04-24 09:01:20 +0100

    first-commit

---
+++ Java/IsaacUtil,java/ArraySortingTask.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class ArraySortingTask{
+
+    public static int[] sortArray(int [] numbers){
+
+        int length =numbers.length();
+
+        for(int firstIndex =0; firstIndex <length; firstIndex++){
+
+            for(int secondIndex =0; secondIndex <length; secondIndex++){
+
+                if (evenNumbers[firstIndex] > evenNumbers[secondIndex]){
+                    int tempSpace =numbers[firstIndex];
+                    numbers[firstIndex] =numbers[secondIndex];
+                    numbers[secondIndex] =tempSpace;
+                }
+
+            }    
+        }
+        
+        return numbers;
+    }
+        
+    public static void findArrayLength(int [] array){
+
+    for (int count =0; count <)
+
+
+
+
+    }
+
+
+
+}
